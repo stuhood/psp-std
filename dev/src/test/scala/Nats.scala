55b06f5ad9a28e0dab4d242c88ecc0287915a237
EECOLOR <eecolor@qirx.org>
2015-02-14 23:55:07 +0100

    Added a method and import to Nats test to make the class compile

--- dev/src/test/scala/Nats.scala
+++ dev/src/test/scala/Nats.scala
@@ -9,7 +9,10 @@ class Nats extends Bundle {
   def ints = NatList((1, 2, 3, 4))
   def strs = NatList(("a", "ab", "abc", "abcd"))
 
+  def bundle = "Some text here, I don't know what Nats means"
+  
   def run(): Boolean = {
+    import NatList._ // It would pick up another implicit
     assert((ints zip ints map (_ + _)).sum == (ints.sum * 2))
     assert((ints zip ints zip ints map (_ + _ + _) sum) == ints.sum * 3)
     assert((ints zip strs zip ints map (_ + _.length + _) sum) == ints.sum * 3)
