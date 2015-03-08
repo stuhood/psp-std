b200873cbff11a971e42be8dc20d97bf4da17cf1
Paul Phillips <paulp@improving.org>
2015-03-07 16:12:22 -0800

    PolicyLoader into pio.

--- std/src/main/scala/Wrappers.scala
+++ std/src/main/scala/Wrappers.scala
@@ -16,39 +16,3 @@ final class LabeledFunction[-T, +R](f: T => R, val to_s: String) extends (T ?=> 
   }
   def apply(x: T): R = f(x)
 }
-
-final class PolicyLoader(val classMap: ExMap[String, Bytes]) extends ClassLoader {
-  private val keys        = classMap.domain.toDirect
-  private val instanceMap = scmMap[String, jClass]()
-  private val errorMap    = scmMap[String, LinkageError]()
-  private def isNoClassDefFoundError(t: Throwable) = t match {
-    case _: NoClassDefFoundError => true
-    case _                       => false
-  }
-
-  def totalClasses = classMap.size
-  def names        = keys
-  def classes      = names map (x => findClass(x))
-  def errors       = errorMap.toMap
-  def missing      = errorMap.m.toExMap filterValues isNoClassDefFoundError keys
-  def otherErrors  = errorMap.m.toExMap filterValues !(isNoClassDefFoundError _)
-  def totalBytes   = classMap.values.map(_.length).foldl(0)(_ + _)
-
-  def define(name: String): jClass               = define(name, classMap(name))
-  def define(name: String, bytes: Bytes): jClass = defineClass(name, bytes, 0, bytes.length, null)
-
-  private def doDefine(name: String): jClass = try define(name) catch {
-    case t: LinkageError => errorMap(name) = t ; null
-    case t: Throwable    => println(s"Caught $t") ; null
-  }
-
-  override def findClass(name: String): jClass = instanceMap.getOrElse(name,
-    findLoadedClass(name) match {
-      case cl: jClass                     => cl
-      case _ if !(classMap contains name) => super.findClass(name)  /** NoClassDefFound */
-      case _                              => doDefine(name) doto (instanceMap(name) = _)
-    }
-  )
-
-  override def toString = s"Loader($totalClasses classes, $totalBytes bytes)"
-}
