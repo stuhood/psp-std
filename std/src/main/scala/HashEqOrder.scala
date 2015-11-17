141a8b0431d25dfc1a007c5a610ac503ee6c00f5
Paul Phillips <paulp@improving.org>
2015-11-17 10:57:30 -0800

    File organizing.

--- std/src/main/scala/HashEqOrder.scala
+++ std/src/main/scala/HashEqOrder.scala
@@ -3,6 +3,30 @@ package std
 
 import api._
 
+object Eq {
+  val Inherited = hash[Any](_ == _)(_.##)
+  val Reference = hash[AnyRef](_ eq _)(_.id_##)
+  val ToString  = inherit[String] on ((x: Any) => "" + x)
+
+  def apply[A](e: Relation[A]): Eq[A]               = new EqImpl[A](e)
+  def hash[A](e: Relation[A])(h: ToInt[A]): Hash[A] = new HashImpl[A](e, h)
+  def reference[A](): Hash[Ref[A]]                  = Reference
+  def inherit[A](): Hash[A]                         = Inherited
+
+  final class HashImpl[-A](e: Relation[A], h: ToInt[A]) extends api.Hash[A] {
+    def equiv(x: A, y: A) = e(x, y)
+    def hash(x: A): Int   = h(x)
+  }
+  final class EqImpl[-A](val e: Relation[A]) extends AnyVal with Eq[A] {
+    def equiv(x: A, y: A) = e(x, y)
+  }
+
+  class EqComparator[A: Eq]() extends Comparator[A] {
+    def compare(x: A, y: A): Int = if (x === y) 0 else x.id_## - y.id_##
+  }
+  def eqComparator[A: Eq](): Comparator[A] = new EqComparator[A]
+}
+
 object Order {
   import Cmp._
 
@@ -22,9 +46,4 @@ object Order {
   final class FromComparator[A](c: Comparator[A])     extends Impl[A]((x, y) => longCmp(c.compare(x, y)))
   final class FromRelation[A](f: OrderRelation[A])    extends Impl[A](f)
   final class ToOrdering[A](z: Order[A])              extends Ordering[A] { def compare(x: A, y: A): Int = z.compare(x, y).intValue }
-}
-
-object Empty {
-  def apply[A](empty: A): Impl[A] = new Impl[A](empty)
-  final class Impl[A](val empty: A) extends AnyVal with Empty[A]
 }
