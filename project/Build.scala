9d38c789a42ce93483cf29eb744cc60d1f35fbf0
Paul Phillips <paulp@improving.org>
2015-11-22 19:58:38 -0800

    Build/coverage experiment.

--- project/Build.scala
+++ project/Build.scala
@@ -39,6 +39,7 @@ object Build extends sbt.Build {
 
   implicit class ProjectOps(val p: Project) {
     def aggregatesAll = p aggregate (projectRefs: _*)
+    def dependsOnAll  = p dependsOn (classpathDeps: _*)
     def allSources    = Def task (sources in Test in p).value ++ (sources in Compile in p).value
     def usesCompiler  = p settings (libraryDependencies += Deps.scalaCompiler.value)
     def usesReflect   = p settings (libraryDependencies += Deps.scalaReflect.value)
@@ -50,8 +51,10 @@ object Build extends sbt.Build {
     def helper(): Project            = p.hidden.noArtifacts setup s"helper project ${p.id}" dependsOn (classpathDeps: _*)
   }
 
+  // resolvers +=  "Jcenter" at "http://jcenter.bintray.com"
+  // updateOptions ~=  (_ withCachedResolution true)
   private def commonSettings(p: Project) = standardSettings ++ Seq(
-                               resolvers +=  Resolver.mavenLocal,
+                       externalResolvers :=  Seq("google" at "http://maven-central.storage.googleapis.com"),
                                  version :=  sbtBuildProps.buildVersion,
                             scalaVersion :=  scalaVersionLatest,
                       crossScalaVersions :=  Seq(scalaVersion.value),
@@ -59,27 +62,28 @@ object Build extends sbt.Build {
                             organization :=  pspOrg,
                            scalacOptions ++= scalacOptionsFor(scalaBinaryVersion.value) ++ stdArgs,
                         triggeredMessage :=  Watched.clearWhenTriggered,
-                       publishMavenStyle :=  true,
-   javaVersionPrefix in javaVersionCheck :=  Some("1.7")
-  ) ++ (
-    if (p.id == "root") Nil
-    else Seq(target <<= javaCrossTarget(p.id))
-  )
+   javaVersionPrefix in javaVersionCheck :=  Some("1.8")
+  ) ++ ( if (p.id == "root") Nil else Seq(target <<= javaCrossTarget(p.id)) )
 
-  lazy val root = project.root.setup dependsOn (classpathDeps: _*) settings (
-                  test <<=  test in testOnly,
+
+  lazy val root = project.root.setup.aggregatesAll.dependsOnAll settings (
     console in Compile <<=  console in Compile in consoleOnly,
        console in Test <<=  console in Test in consoleOnly,
           watchSources <++= testOnly.allSources,
-          watchSources <++= consoleOnly.allSources
-  )
+          watchSources <++= consoleOnly.allSources,
+                  test <<=  test in testOnly
+  ) also addCommandAlias("cover", "; clean ; coverage ; test ; coverageReport")
 
-  lazy val consoleOnly = project.helper.usesCompiler.usesAmmonite dependsOn (testOnly % "test->test") deps (consoleDependencies: _*)
-  lazy val testOnly    = project.helper aggregate (projectRefs: _*) settings (
-          testOptions in Test  +=  Tests.Argument(TestFrameworks.ScalaCheck, "-verbosity", "1"),
-    parallelExecution in Test  :=  false,
-                  logBuffered  :=  false,
-          libraryDependencies <++= testDependencies,
-                         test  :=  (run in Test toTask "").value
+  lazy val consoleOnly = (
+    project.helper.usesCompiler.usesAmmonite.dependsOnAll
+      dependsOn (testOnly % "test->test")
+           deps (consoleDependencies: _*)
+  )
+  lazy val testOnly = project.helper.dependsOnAll.aggregatesAll settings (
+     testOptions in Test  +=  Tests.Argument(TestFrameworks.ScalaCheck, "-verbosity", "1"),
+             logBuffered  :=  false,
+     libraryDependencies  +=  scalacheck,
+     libraryDependencies <++= testDependencies,
+                    test  :=  (run in Test toTask "").value
   )
 }
